b601b524af298d1358166cc95e6bfb61293bba48
psevdo:Proger <psevdoproger@yandex.ru>
2022-04-23 17:03:17 +0300

    fix: small fixies

--- lua/mappings/defaults.lua
+++ lua/mappings/defaults.lua
@@ -14,10 +14,11 @@ map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", default_opts)
 -- Conqueror of Competetion keys
 ----------------------------------------------------------
 map("n", "K", [[<cmd>call CocAction("doHover")<cr>]], default_opts)
-map("n", "<CR>", [[<CMD>CodeActionMenu<CR>]], default_opts)
+map("n", "<CR>", [[<CMD>CocAction<CR>]], default_opts)
 ----------------------------------------------------------
 -- IntelliJ-like keys
 ----------------------------------------------------------
+map("n", "`<Tab>", [[<CMD>Telescope buffers<CR>]], default_opts)
 map("n", "``", [[<CMD>lua require"lir.float".toggle()<CR>]], default_opts)
 map("n", "`1", [[<CMD>Telescope fd<CR>]], default_opts)
 map("n", "`2", [[<CMD>Telescope live_grep<CR>]], default_opts)
