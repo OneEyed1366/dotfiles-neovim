545cf40e193c29cc9b1d300d895221ba27b08a64
psevdo:Proger <psevdoproger@yandex.ru>
2022-05-19 10:09:03 +0300

    feat: add autoformat

--- lua/plugins/lsp-and-some-necessary-stuff.lua
+++ lua/plugins/lsp-and-some-necessary-stuff.lua
@@ -2,18 +2,13 @@ require("packer").use {
     "neovim/nvim-lspconfig",
     config = function()
         local map = vim.api.nvim_set_keymap
-        local opts = {noremap = true, silent = true}
+        local opts = { noremap = true, silent = true }
         local servers = { "pyright", "tsserver" }
         local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
         -- after the language server attaches to the current buffer
         local on_attach = function(client, bufnr)
-          -- Enable completion triggered by <c-x><c-o>
+            -- Enable completion triggered by <c-x><c-o>
             vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-            -- map(bufnr, 'n', '<CR>', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-            -- map(bufnr, 'n', '`r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-            -- map(bufnr, 'n', '`a', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-            -- map(bufnr, 'n', '`s', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
         end
         -- Mappings.
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -26,39 +21,20 @@ require("packer").use {
         map('n', '`s', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 
         for _, lsp in pairs(servers) do
-          require("lspconfig")[lsp].setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            flags = {
-              debounce_text_changes = 150,
+            require("lspconfig")[lsp].setup {
+                on_attach = on_attach,
+                capabilities = capabilities,
+                flags = {
+                    debounce_text_changes = 150,
+                }
             }
-          }
         end
     end,
     requires = {
-        -- {"neoclide/coc.nvim", 
-        --    branch ="master",
-        --    run = "yarn install --frozen-lockfile",
-        --    config = function()
-        --    --    map("n", "", [[<CMD>lua require("coc-references")<CR>]], opts)
-        --         vim.cmd[[
-        --         """GoTo errors"""
-        --         nmap <silent> `q <Plug>(coc-diagnostic-prev)
-        --         nmap <silent> `w <Plug>(coc-diagnostic-next)
-        --
-        --         """GoTo Code navigation"""
-        --         nmap <silent> `a <Plug>(coc-definition)
-        --         nmap <silent> `s <Plug>(coc-type-definition)
-        --         nmap <silent> `z <Plug>(coc-implementation)
-        --         nmap <silent> `x <Plug>(coc-references)
-        --         """Rename smth"""
-        --         nmap `r <Plug>(coc-rename)
-        --         ]]
-        --     end},
-        {"akinsho/flutter-tools.nvim", requires = {{"nvim-lua/plenary.nvim"}}, config = function ()
-            require("flutter-tools").setup{}
-        end},
-        {"williamboman/nvim-lsp-installer",
+        { "akinsho/flutter-tools.nvim", requires = { { "nvim-lua/plenary.nvim" } }, config = function()
+            require("flutter-tools").setup {}
+        end },
+        { "williamboman/nvim-lsp-installer",
             config = function()
                 require("nvim-lsp-installer").on_server_ready(function(server)
                     local opts = {}
@@ -67,56 +43,56 @@ require("packer").use {
                 end)
             end,
         },
-        {"L3MON4D3/LuaSnip"},
-        {"hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require("cmp")
+        { "L3MON4D3/LuaSnip" },
+        { "hrsh7th/nvim-cmp",
+            config = function()
+                local cmp = require("cmp")
 
-            vim.o.completeopt = "menu,menuone,noselect"
-                    
-            cmp.setup({
-                snippet = {
-                  expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                  end,
-                },
-                window = {
-                  completion = cmp.config.window.bordered(),
-                  documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                  -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                  -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                  -- ['<C-Space>'] = cmp.mapping.complete(),
-                  -- ['<C-e>'] = cmp.mapping.abort(),
-                  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                }),
-                sources = cmp.config.sources({
-                  { name = 'nvim_lsp' },
-                  { name = 'luasnip' }, -- For luasnip users.
-                }, {
-                  { name = 'buffer' },
+                vim.o.completeopt = "menu,menuone,noselect"
+
+                cmp.setup({
+                    snippet = {
+                        expand = function(args)
+                            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        end,
+                    },
+                    window = {
+                        completion = cmp.config.window.bordered(),
+                        documentation = cmp.config.window.bordered(),
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                        -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                        -- ['<C-Space>'] = cmp.mapping.complete(),
+                        -- ['<C-e>'] = cmp.mapping.abort(),
+                        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    }),
+                    sources = cmp.config.sources({
+                        { name = 'nvim_lsp' },
+                        { name = 'luasnip' }, -- For luasnip users.
+                    }, {
+                        { name = 'buffer' },
+                    })
                 })
-              })
             end
         },
-        {"hrsh7th/cmp-buffer"},
-        {"hrsh7th/cmp-nvim-lsp"},
-        {"nvim-treesitter/nvim-treesitter",
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "nvim-treesitter/nvim-treesitter",
             run = ":TSUpdate",
             config = function()
-                require'nvim-treesitter.configs'.setup {
-                  indent = {
-                    enable = true
-                  },
-                  highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                  },
+                require 'nvim-treesitter.configs'.setup {
+                    indent = {
+                        enable = true
+                    },
+                    highlight = {
+                        enable = true,
+                        additional_vim_regex_highlighting = false,
+                    },
                 }
             end
         },
-        {"lukas-reineke/indent-blankline.nvim",
+        { "lukas-reineke/indent-blankline.nvim",
             config = function()
                 require("indent_blankline").setup {
                     -- for example, context is off by default, use this to turn it on
@@ -125,22 +101,22 @@ require("packer").use {
                 }
             end
         },
-        {"windwp/nvim-autopairs",
+        { "windwp/nvim-autopairs",
             config = function()
-                require("nvim-autopairs").setup{}
+                require("nvim-autopairs").setup {}
             end
         },
-        {"windwp/nvim-ts-autotag",
+        { "windwp/nvim-ts-autotag",
             config = function()
                 require("nvim-ts-autotag").setup()
-            end},
-        {"numToStr/Comment.nvim",
+            end },
+        { "numToStr/Comment.nvim",
             config = function()
                 require("Comment").setup()
-            end},
-        {"norcalli/nvim-colorizer.lua",
+            end },
+        { "norcalli/nvim-colorizer.lua",
             config = function() require("colorizer").setup() end
         }
     },
-    
+
 }
--- lua/settings/defaults.lua
+++ lua/settings/defaults.lua
@@ -8,28 +8,19 @@ global.relativenumber = true
 -- global.encoding = "utf-8"
 global.guifont = "hack_nerd_font:h22"
 vim.diagnostic.config({
-  virtual_text = false,
+    virtual_text = false,
 })
-cmd[[
+
+cmd [[
 colorscheme onedark
 
-autocmd Filetype json setlocal ts=4 sw=4 sts=0 expandtab
-autocmd Filetype yaml setlocal ts=4 sw=4 sts=0 expandtab
+autocmd BufWritePre * lua vim.lsp.buf.formatting()
 
-autocmd Filetype html setlocal ts=2 sw=2 expandtab
-autocmd Filetype css setlocal ts=2 sw=2 expandtab
-autocmd Filetype scss setlocal ts=2 sw=2 expandtab
-autocmd Filetype less setlocal ts=2 sw=2 expandtab
-autocmd Filetype stylus setlocal ts=2 sw=2 expandtab
-autocmd Filetype javascript setlocal ts=2 sw=2 sts=0 expandtab
-autocmd Filetype typescript setlocal ts=2 sw=2 sts=0 expandtab
-autocmd Filetype javascriptreact setlocal ts=2 sw=2 sts=0 expandtab
-autocmd Filetype typescriptreact setlocal ts=2 sw=2 sts=0 expandtab
-autocmd Filetype dart setlocal ts=2 sw=2 sts=0 expandtab
-autocmd Filetype vue setlocal ts=2 sw=2 sts=0 expandtab
+autocmd Filetype json,yaml setlocal ts=4 sw=4 sts=0 expandtab
 
-autocmd Filetype python setlocal ts=4 sw=4 sts=0 expandtab
-autocmd Filetype php setlocal ts=4 sw=4 sts=0 expandtab
-autocmd Filetype lua setlocal ts=4 sw=4 sts=0 expandtab
-autocmd Filetype vim setlocal ts=4 sw=4 sts=0 expandtab
+autocmd Filetype html,css,scss,less,stylus setlocal ts=2 sw=2 expandtab
+
+autocmd Filetype dart,javascript,javascriptreact,typescript,typescriptreact,vue setlocal ts=2 sw=2 sts=0 expandtab
+
+autocmd Filetype python,php,lua,vim setlocal ts=4 sw=4 sts=0 expandtab
 ]]
