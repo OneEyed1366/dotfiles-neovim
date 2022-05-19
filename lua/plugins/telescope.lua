c750239eaef11567be235f66421f073e62823e3d
psevdo:Proger <psevdoproger@yandex.ru>
2022-05-19 23:10:18 +0300

    feat: improve Telescope mappings for git

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,16 @@
 require("packer").use {
     "nvim-telescope/telescope.nvim",
     requires = {
-        {"nvim-lua/plenary.nvim"},
-    }
+        { "nvim-lua/plenary.nvim" },
+    },
+    config = function()
+        local map = vim.api.nvim_set_keymap
+        local opts = { noremap = true, silent = true }
+
+        map("n", "==", "<CMD>Telescope git_status<CR>", opts)
+        map("n", "-=", "<CMD>Telescope git_branches<CR>", opts)
+        map("n", "0=", "<CMD>Telescope git_commits<CR>", opts)
+        map("n", "9=", "<CMD>Telescope git_stash<CR>", opts)
+        map("n", "8=", "<CMD>Telescope git_files<CR>", opts)
+    end
 }
