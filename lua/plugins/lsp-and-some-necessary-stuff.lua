719318faeeae72a4c803dd1da2bfc7dadb772480
psevdo:Proger <psevdoproger@yandex.ru>
2022-05-22 16:54:28 +0300

    fix: removed mini.competition & restore nvim-cmp

--- lua/plugins/lsp-and-some-necessary-stuff.lua
+++ lua/plugins/lsp-and-some-necessary-stuff.lua
@@ -4,7 +4,7 @@ require("packer").use {
         local map = vim.api.nvim_set_keymap
         local opts = { noremap = true, silent = true }
         local servers = { "pyright", "tsserver" }
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
         -- after the language server attaches to the current buffer
         local on_attach = function(client, bufnr)
             -- Enable completion triggered by <c-x><c-o>
@@ -18,8 +18,8 @@ require("packer").use {
         map("n", "`w", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
         map('n', '`r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
         -- Basic LSP stuff
-        map("n", "`a", "<CMD>lua vim.lsp.buf.definition<CR>", opts)
-        map("n", "`s", "<CMD>lua vim.lsp.buf.type_definition<CR>", opts)
+        map("n", "`a", "<CMD>lua vim.lsp.buf.definition()<CR>", opts)
+        map("n", "`s", "<CMD>lua vim.lsp.buf.type_definition()<CR>", opts)
 
         for _, lsp in pairs(servers) do
             require("lspconfig")[lsp].setup {
@@ -45,6 +45,52 @@ require("packer").use {
                 end)
             end,
         },
+        { "L3MON4D3/LuaSnip" },
+        { "hrsh7th/nvim-cmp",
+            config = function()
+                local cmp = require("cmp")
+
+                vim.o.completeopt = "menu,menuone,noselect"
+
+                cmp.setup({
+                    snippet = {
+                        expand = function(args)
+                            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        end,
+                    },
+                    window = {
+                        completion = cmp.config.window.bordered(),
+                        documentation = cmp.config.window.bordered(),
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                        -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                        ["<Tab>"] = cmp.mapping(function(fallback)
+                            -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+                            if cmp.visible() then
+                                local entry = cmp.get_selected_entry()
+                                if not entry then
+                                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                                else
+                                    cmp.confirm()
+                                end
+                            else
+                                fallback()
+                            end
+                        end, { "i", "s", "c", }),
+                        -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    }),
+                    sources = cmp.config.sources({
+                        { name = 'nvim_lsp' },
+                        { name = 'luasnip' }, -- For luasnip users.
+                    }, {
+                        { name = 'buffer' },
+                    })
+                })
+            end
+        },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-nvim-lsp" },
         { "nvim-treesitter/nvim-treesitter",
             run = ":TSUpdate",
             config = function()
@@ -60,7 +106,6 @@ require("packer").use {
             end
         },
         {"echasnovski/mini.nvim", branch = "stable", config = function()
-            require("mini.completion").setup();
             require("mini.comment").setup();
             require("mini.indentscope").setup();
             require("mini.pairs").setup();
@@ -76,6 +121,9 @@ require("packer").use {
         },
         { "sbdchd/neoformat", config = function()
             vim.cmd[[
+                let g:neoformat_run_all_formatters = 1
+                let g:neoformat_try_node_exe = 1
+
                 augroup fmt
                     autocmd!
                     autocmd BufWritePre * undojoin | Neoformat
