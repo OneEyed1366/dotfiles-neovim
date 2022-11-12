9b3a9177fcc254af1f239a6b22c4aaa388131c88
OneEyed1366 <psevdoproger@gmail.com>
2022-11-12 21:26:11 +0300

    update

--- lua/user/modules/features/basic_format_on_save/init.lua
+++ lua/user/modules/features/basic_format_on_save/init.lua
@@ -9,11 +9,13 @@ neoformat.packages = {
 neoformat.configs = {
   ["neoformat"] = function()
     vim.cmd[[
-      " let g:neoformat_run_all_formatters = 1
-      " let g:neoformat_try_node_exe = 1
+      let g:neoformat_run_all_formatters = 1
+      let g:neoformat_try_node_exe = 0
       let g:neoformat_enables_html = ['prettier']
       let g:neoformat_enabled_javascript = ['prettier', 'eslint']
+      let g:neoformat_enabled_javascriptreact = ['prettier', 'eslint']
       let g:neoformat_enables_typescript = ['prettier', 'prettier-eslint', 'tslint', 'eslint_d']
+      let g:neoformat_enabled_typescriptreact = ['prettier', 'eslint']
       let g:neoformat_enables_svelte = ['prettier-plugin-svelte', 'prettierd']
       let g:neoformat_enables_vue = ['prettierd', 'prettier']
       let g:neoformat_enables_graphql = ['prettier']
