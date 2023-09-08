a841e4a7dfa55d0da70b1f3df5db794f4b3f60f9
OneEyed1366 <psevdoproger@gmail.com>
2023-09-08 08:25:35 +0300

    feat: add keybindings for git

--- lua/vscode-plugins/git.lua
+++ lua/vscode-plugins/git.lua
@@ -2,7 +2,10 @@ return {
   ["]g"] = { "<CMD>call VSCodeNotify('workbench.action.editor.nextChange')<CR>", "git Go to next chunk" },
   ["[g"] = { "<CMD>call VSCodeNotify('workbench.action.editor.previousChange')<CR>", "git Go to prev chunk" },
   ["<leader>g"] = {
-    ["<leader>"] = { "<CMD>call VSCodeNotify('git.viewFileHistory)<CR>", "Git file history" },
+    ["<leader>"] = { "<CMD>call VSCodeNotify('gitlens.openFileHistory')<CR>", "Git" },
+    ["/"] = { "<CMD>call VSCodeNotify('gitlens.showCommitSearch')<CR>", "Git search in commits" },
+    ["."] = { "<CMD>call VSCodeNotify('git.checkout')<CR>", "Git branches" },
+    [","] = { "<CMD>call VSCodeNotify('git.merge')<CR>", "Git merge into current branch" },
     ["m"] = { "<CMD>call VSCodeNotify('git.stage)<CR>", "Git stage stanges" },
     ["u"] = { "<CMD>call VSCodeNotify('git.unstage')<CR>", "Git Undo" },
     ["i"] = { "<CMD>call VSCodeNotify('git.viewLineHistory')<CR>",
