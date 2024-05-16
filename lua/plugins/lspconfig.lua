6008fbf679d1bca4560ec6d021e134c51b1aed4e
Prokopenko, Andrey <psevdoproger@gmail.com>
2024-05-16 09:59:06 +0300

    refactor: add "ensure_installed" for default servers, remove unnecessary

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -11,9 +11,7 @@ return {
                 local mason_lspconfig = require('mason-lspconfig')
                 local capabilities = vim.lsp.protocol.make_client_capabilities()
                 local servers = mason_lspconfig.get_installed_servers()
-                -- If you are using mason.nvim, you can get the ts_plugin_path like this
-                local mason_registry = require('mason-registry')
-                local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+                local vue_language_server_path = '/node_modules/@vue/language-server'
 
                 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
