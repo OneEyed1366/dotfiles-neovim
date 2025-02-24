658f4e0b2a03e534312735dcbfbb09c0c4e316b8
Andrey Prokopenko <andrprokopenko@ozon.ru>
2025-02-24 09:43:56 +0300

    [master] refactor: many updates;

--- init.lua
+++ init.lua
@@ -16,9 +16,11 @@ vim.opt.rtp:prepend(lazypath)
 
 if vim.g.vscode then
   local which_key = require("vscode-plugins.whichkey")
+  local hop = require("plugins.hop")
 
   require("lazy").setup({
     which_key,
+    hop,
   }, {})
 else
   -- local neo_tree = require("plugins.neo-tree")
@@ -31,6 +33,7 @@ else
   -- local usages = require("plugins.usages")
   -- local chat_gpt = require("plugins.chat-gpt")
   -- local format_on_save = require("plugins.format-on-save")
+  -- local persisted = require("plugins.persisted")
   -- local lsp_timeout = require("plugins.lsp-timeout")
 
   local wtf = require("plugins.wtf")
@@ -40,8 +43,8 @@ else
   local lspconfig = require("plugins.lspconfig")
   local scope = require("plugins.scope")
   local git = require("plugins.git")
-
-  local persisted = require("plugins.persisted")
+  local precognition = require("plugins.precognition")
+  local hop = require("plugins.hop")
 
   local telescope = require("plugins.telescope")
   local docker_telescope = require("plugins.telescope.docker_telescope")
@@ -63,6 +66,9 @@ else
   local floaterm = require("plugins.terminal")
   local twilight = require("plugins.twilight")
   local noice = require("plugins.noice")
+  local oil_nvim = require("plugins.oil_nvim")
+  local none_ls = require("plugins.none-ls")
+  local none_ls_autoload = require("plugins.none-ls-autoload")
 
 
   require("lazy").setup({
@@ -76,9 +82,13 @@ else
     -- dap,
     -- format_on_save,
     -- Причина: после 2-4 включений-выключений LSP, глючат LSP сервисы и, возможно, сильно нагружается процессор
-    -- lsp_timeout
+    -- lsp_timeout,
+    -- Причина выключения: иногда Telescope FileBrowser не может корректно распарсить директории. SessionDelete не помогает
+    -- persisted,
+    -- precognition,
+    hop,
+    oil_nvim,
     noice,
-    persisted,
     git,
     wtf,
     git_telescope,
@@ -100,5 +110,7 @@ else
     window_transparent,
     floaterm,
     scope,
+    none_ls,
+    none_ls_autoload,
   }, {})
 end
--- lua/configs/keymappings.lua
+++ lua/configs/keymappings.lua
@@ -14,47 +14,14 @@ return {
       { "-",  "<CMD>split<CR><C-W>j",  name = "Split horizontal" },
     },
   },
-  -- Telescope
-  -- { "<leader><CR>", "<CMD>Telescope commands<CR>", name = "All commands" },
-  -- FS
-  -- { "<leader>.", "<CMD>Telescope file_browser path=%:p:h<CR>", name = "File Browser" },
-  -- { "<leader>M", "<CMD>w!<CR>", name = "Force Write" },
-  -- Git
-  -- { "[", {
-  --   { "g", '<CMD>lua require"gitsigns".prev_hunk()<CR>', name = "Prev diff" },
-  -- } },
-  -- { "]", {
-  --   { "g", '<CMD>lua require"gitsigns".next_hunk()<CR>', name = "Next diff" },
-  -- } },
-  -- {
-  --   "<leader>g",
-  --   name = "+git",
-  --   {
-  --     {
-  --       "C",
-  --       name = "+Commit",
-  --       {
-  --         { "c", "<CMD>terminal git commit<CR>", name = "commit" },
-  --         { "a", "<CMD>terminal git commit --amend<CR>", name = "amend" },
-  --       },
-  --     },
-  --     { "p", "<cmd>terminal git pull<CR>", name = "Pull" },
-  --     { "P", "<cmd>terminal git push<CR>", name = "Pull" },
-  --     { "m", '<cmd>lua require"gitsigns".stage_hunk()<CR>', name = "Stage hunk" },
-  --     { "M", '<cmd>lua require"gitsigns".stage_buffer()<CR>', name = "Stage buffer" },
-  --     { "u", '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>', name = "Unstage" },
-  --     { "x", '<cmd>lua require"gitsigns".reset_hunk()<CR>', name = "Reset hunk" },
-  --     { "X", '<cmd>lua require"gitsigns".reset_buffer()<CR>', name = "Reset buffer" },
-  --     { ",", '<cmd>lua require"gitsigns".preview_hunk()<CR>', name = "Preview" },
-  --     { "B", "<cmd>Telescope git_branches<CR>", name = "Branches" },
-  --   },
-  -- },
   -- Buffers
   {
     "<leader>b",
     name = "+buffer",
     {
       { "D", "<CMD>bdelete!<CR>", name = "Force Delete" },
+      { "a", "<CMD>%bd|e#<CR>", name = "Close all except opened" },
+      { "A", "<CMD>bufdo bd<CR>", name = "Close all buffers" },
     }
   },
   -- Code
